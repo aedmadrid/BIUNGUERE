45b6cfd64fdffba8c1a7f8ce0a9cd99e3c9a2400
lucasengithub <86058446+lucasengithub@users.noreply.github.com>
2025-12-16 10:41:57 +0100

    hola

---
+++ web/a.js
@@ -0,0 +1,9 @@
+function gotoDrive() {
+  alert(
+    "Atención!\nEste enlace es solo para correción docente. Este es un proyecto que no está listo para ser usado. Estamos en una fase primeriza. Accede solo si eres Fátima ",
+  );
+  window.open(
+    "https://drive.google.com/drive/folders/10kkHRvMtsmFDpqN9nXcGvmVfOZagMaoB?usp=sharing",
+    "_blank",
+  );
+}
