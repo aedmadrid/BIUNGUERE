6b6b69a614f8b25994f97be261a0d2bf2abfe56f
lucasengithub <86058446+lucasengithub@users.noreply.github.com>
2025-12-16 11:00:17 +0100

    ececwcew

--- web/a.js
+++ web/a.js
@@ -1,6 +1,6 @@
 function gotoDrive() {
   alert(
-    "Atención!\nEste enlace es solo para correción docente. Este es un proyecto que no está listo para ser usado. Estamos en una fase primeriza. Accede solo si eres Fátima ",
+    "Atención!\nEste enlace es solo para correción docente. Este es un proyecto que no está listo para ser usado. Estamos en una fase primeriza. Accede solo si eres Fatima Ferreiro Galguera ",
   );
   window.open(
     "https://drive.google.com/drive/folders/10kkHRvMtsmFDpqN9nXcGvmVfOZagMaoB?usp=sharing",
